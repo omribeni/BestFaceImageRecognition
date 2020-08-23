11213ee60a8c2e82a6829a755f2f7b290c1cdd5b
Omri Ben-Ishai <omribenishai015@gmail.com>
2020-08-23 12:24:09 +0300

    fix all faces detected global var passing

--- src/Program.py
+++ src/Program.py
@@ -36,7 +36,8 @@ def GetBestImage():
                 all_images_faces.extend(cur_image_faces)
 
         if all_images_faces:
-            biggest_person_group = get_biggest_person_group(all_images_faces)
+            temp_faces_list = all_images_faces
+            biggest_person_group = get_biggest_person_group(temp_faces_list)
 
             best_image = find_largest_person_image(biggest_person_group,all_images_faces,face_image_resolution)
             if best_image:
@@ -52,28 +53,26 @@ def GetBestImage():
 
 def get_biggest_person_group(faces_list):
     biggest_person_group = []
-    while temp_faces_list:
-        face = temp_faces_list[0]
-        cur_group = azure_face_api_client.recognize_similar_faces(face, temp_faces_list[1:])
+    while faces_list:
+        face = faces_list[0]
+        cur_group = azure_face_api_client.recognize_similar_faces(face, faces_list[1:])
         if cur_group and len(cur_group) > len(biggest_person_group):
             biggest_person_group = cur_group
             biggest_person_group.append(face)
-            temp_faces_list = [x for x in temp_faces_list if x not in cur_group]
-        temp_faces_list = temp_faces_list[1:]
+            faces_list = [x for x in faces_list if x not in cur_group]
+        faces_list = faces_list[1:]
     return biggest_person_group
 
 
-
-
-def find_largest_person_image(person_faces ,all_images_faces, face_image_resolution):
+def find_largest_person_image(person_faces ,all_faces, image_resolution):
     best_image = None
     best_resolution = 0
 
     for fa in person_faces():
         # get the detectedFace object form the similarFace list by face_id
-        detected_face = next((x for x in all_images_faces if x.face_id == fa.face_id), None)
+        detected_face = next((x for x in all_faces if x.face_id == fa.face_id), None)
         if detected_face:
-            cur_res = face_image_resolution[detected_face.face_id]
+            cur_res = image_resolution[detected_face.face_id]
             picture_surface_area = cur_res[0] * cur_res[1]
             face_box_surface_area = detected_face.face_rectangle.height * detected_face.face_rectangle.width
             if face_box_surface_area / picture_surface_area > best_resolution:
